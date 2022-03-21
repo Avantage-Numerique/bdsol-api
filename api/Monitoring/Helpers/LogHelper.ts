2c8385abecd8c256d2a200e1796d90568360cb4d
Marc-André Martin <marcandre@mamarmite.com>
2022-03-21 13:09:01 -0400

    Ajout de la structure pour ajouter plus tard un système de log dans un fichier et/ou sur la console.

--- api/Monitoring/Helpers/LogHelper.ts
+++ api/Monitoring/Helpers/LogHelper.ts
@@ -4,7 +4,14 @@
  */
 export default class LogHelper
 {
-    public static log(message:string) {
-        console.log(message);
+    public static log(message:string, printToConsole:boolean=true) {
+        if (printToConsole) {
+            console.log(message);
+        }
+        LogHelper.logToFile(message);
+    }
+
+    public static logToFile(message:string) {
+        return true;
     }
 }
