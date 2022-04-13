7ac032d0a86bd34d4a0000fb18004c93b1d31fad
SonofaPancak <epaulard21@hotmail.com>
2022-04-13 15:52:06 -0400

    Ajout de Log et ébauche de fonction CRUD avec les routes pour Personne

--- api/src/Personnes/Controllers/PersonneController.ts
+++ api/src/Personnes/Controllers/PersonneController.ts
@@ -4,10 +4,41 @@ import PersonneService from "../Services/PersonneService";
 
 class PersonneController {
 
-    public async create():Promise<string> {
-        LogHelper.log("Début de création d'une personne");
+    public async create(nom:string, prenom:string, surnom:string):Promise<void> {
+        LogHelper.log(`Début de création d'une personne avec les paramètre suivants: nom=${nom}, prenom=${prenom}, surnom=${surnom}`);
         LogHelper.log("Réussite de la création d'une personne");
-        return "Bidon";
+        LogHelper.error("Échec de la création d'une personne");
+        return;
+    }
+
+    
+    public async list():Promise<void> {
+        LogHelper.log("Début de la requête d'obtention de la liste de personne");
+        LogHelper.log("Réussite de la requête d'obtention de la liste");
+        LogHelper.log("Échec de la requête d'obtention de la liste");
+        return;
+    }
+
+    public async find():Promise<void> {
+        LogHelper.log("Début de la recherche dans la liste");
+        LogHelper.log("X résultat trouvé");
+        LogHelper.log("Aucun résultat trouvé");
+        LogHelper.log("Échec de la recherche. Cause : ______");
+        return;
+    }
+
+    public async update():Promise<void> {
+        LogHelper.log("Début de la tentative d'update les données d'une personne");
+        LogHelper.log("Réussite de l'update des données d'une personne");
+        LogHelper.log("Échec de l'update des données d'une personne");
+        return;
+    }
+
+    public async delete():Promise<void> {
+        LogHelper.log("Début de la suppression d'une personne");
+        LogHelper.log("Réussite de la suppression d'une personne");
+        LogHelper.log("Échec de la suppression d'une personne");
+        return;
     }
 
 }
--- api/src/Personnes/Routes/PersonneRoutes.ts
+++ api/src/Personnes/Routes/PersonneRoutes.ts
@@ -7,27 +7,47 @@ const PersonneRouter = express.Router();
 
 
 //POST Create new personne
-PersonneRouter.post('/create', (req, res) => {
-    LogHelper.log("Demande de création d'une Personne");
+PersonneRouter.post('/create', async (req, res) => {
+    LogHelper.log("Demande de création d'une personne");
 
-    //let PersonneSchema personne = new PersonneSchema()??
+    let {nom, prenom, surnom} = req.body;
     
+    const controller = new PersonneController();
+    const response = await controller.create(nom, prenom, surnom);
+    return res.status(200).send(response);
+});
+
+//POST Find une personne dans la liste
+PersonneRouter.post('/find', async (req, res) => {
+    LogHelper.log("Demande de recherche dans la liste de personnes");
+});
+
+//POST Read/List des personnes
+PersonneRouter.post('/list', async (req, res) => {
+    LogHelper.log("Demande d'accès à la liste de personnes");
 
     const controller = new PersonneController();
-    const response = await controller.create();
-    return res.status(response).send(response);
-});
-
-//POST Find une personne
-PersonneRouter.post('/find', (req, res) => {
-
-});
-
-//POST Read/List les personne
-PersonneRouter.get('/find')
+    const response = await controller.list();
+    //return
+})
 
 //POST UPDATE une personne
+PersonneRouter.post('/update', async (req, res) => {
+    LogHelper.log("Demande d'update des données d'une personne");
+
+    const controller = new PersonneController();
+    const response = await controller.update();
+    //return
+})
 
 //POST-->Delete http? Delete une personne
+PersonneRouter.delete('/delete', async (req, res) => {
+    LogHelper.log("Demande de suppression d'une personne");
+
+    const controller = new PersonneController();
+    const response = await controller.delete();
+    //return
+})
+
 
 export default PersonneRouter;
