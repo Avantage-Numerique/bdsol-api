d081007aaed575074cdacb7d6a75f6f64383be43
SonofaPancak <epaulard21@hotmail.com>
2022-04-19 10:38:02 -0400

    Modification d'implémentation de l'entité personne vers l'utilisation d'une classe, et finalisation de l'ébauche create() et update() de la route /personne

--- api/src/Personnes/Controllers/PersonneController.ts
+++ api/src/Personnes/Controllers/PersonneController.ts
@@ -1,19 +1,79 @@
 import LogHelper from "../../Monitoring/Helpers/LogHelper";
-import ServerController from "../../Server/Controllers/ServerController";
-import PersonneService from "../Services/PersonneService";
 import Personne from "../Models/Personne"
+//import { PersonneSchema } from "../Schemas/PersonneSchema";
+import mongoose from "mongoose";
+import PersonneService from "../Services/PersonneService";
 
 class PersonneController {
 
-    public async create(nom:string, prenom:string, surnom:string, description:string):Promise<void> {
-        LogHelper.log("Début de création d'une personne avec les paramètre suivants: "+
-                        `nom=${nom}, prenom=${prenom}, surnom=${surnom}, description=${description}`);
-        LogHelper.log("Réussite de la création d'une personne");
-        LogHelper.error("Échec de la création d'une personne");
+    // Service attribute + constructeur userservice user. getinstance()
+    public service:PersonneService;
+
+    constructor(){
+        this.service = new PersonneService(Personne.getInstance());
+    }
+
+    public async create(pers:Personne):Promise<void> {
+        LogHelper.log("Début de création d'une personne avec les paramètre suivants: " + pers.AttributsPersonneToString());
+
+        try{
+            let createdDocumentResponse = await this.service.insert(pers);
+            if(createdDocumentResponse != undefined) LogHelper.error("createdDocumentResponse : " + createdDocumentResponse.message, createdDocumentResponse.code, createdDocumentResponse.error)
+            LogHelper.log("Réussite de la création d'une personne");
+        }
+        catch(e){
+            LogHelper.error("Échec de la création d'une personne", e);
+        }
+
+        //Demande à la BD la création/vérification de la personne
+
+        //Si all good, add new personne => return all good
+        /*try{
+            //const personneService:PersonneService = new PersonneService(Personne.modelName);
+            //personneService.insert("allo");
+            const personne = mongoose.model(Personne.modelName,Personne.schema);
+            const p = new personne();
+            p._id = new mongoose.Types.ObjectId();
+            p.nom = pers.nom;
+            p.prenom = pers.prenom;
+            p.surnom = pers.surnom;
+            p.description = pers.description;
+            p.markModified(p.nom);
+            await p.save();
+            */
+            //Si all good
+            //LogHelper.log("Réussite de la création d'une personne");
+        //}
+        /*catch(e){
+            //Sinon (Went wrong => return went wrong)
+            LogHelper.error("Échec de la création d'une personne", e);
+        }*/
+
         return;
     }
 
     
+    public async update(id:string, pers:Personne):Promise<void> {
+        LogHelper.log("Début de la tentative d'update les données d'une personne");
+
+        try{
+            //Demande à la BD l'objet personne à modifier
+            let updateDocumentResponse = await this.service.update(id, pers);
+
+            //Si all good, send modif => return all good
+            if (updateDocumentResponse.error == false && updateDocumentResponse.message == "Mise à jour de l'item réussi")
+            LogHelper.warn(updateDocumentResponse.message);
+
+        }
+        catch(e){
+            //Sinon (went wrong => return went wrong)
+            LogHelper.log("Échec de l'update des données d'une personne");
+            return;
+        }
+        LogHelper.log("Réussite de l'update des données d'une personne");
+        return;
+    }
+
     public async list():Promise<void> {
         LogHelper.log("Début de la requête d'obtention de la liste de personne");
         LogHelper.log("Réussite de la requête d'obtention de la liste");
@@ -26,13 +86,6 @@ class PersonneController {
         LogHelper.log("X résultat trouvé");
         LogHelper.log("Aucun résultat trouvé");
         LogHelper.log("Échec de la recherche. Cause : ______");
-        return;
-    }
-
-    public async update():Promise<void> {
-        LogHelper.log("Début de la tentative d'update les données d'une personne");
-        LogHelper.log("Réussite de l'update des données d'une personne");
-        LogHelper.log("Échec de l'update des données d'une personne");
         return;
     }
 
--- api/src/Personnes/Schemas/PersonneSchema.ts
+++ api/src/Personnes/Schemas/PersonneSchema.ts
@@ -1,23 +1,32 @@
+import { Timestamp } from "bson";
 import {Schema, Document} from "mongoose"
+import PersonneService from "../Services/PersonneService";
 /**
  *
  *
  */
 
 export interface PersonneSchema extends Document {
+    _id:Schema.Types.ObjectId;
     nom:string;
     prenom:string;
     surnom:string;
     description:string
 
 }
-
-export const PersonneSchema = new Schema<PersonneSchema>({
-        nom: { type: String, required: true },
-        prenom: { type: String, required: true},
-        surnom: String,
-        description: String
-    },
-        {
-            timestamps: true
+/*
+export const PersonneSchema = new Schema­<PersonneSchema>({
+    _id: Schema.Types.ObjectId,
+    nom: { type: String, required: true},
+    prenom: { type: String, required: true },
+    surnom: String,
+    description: String
+};
+    {
+        timestamps: true
 });
+
+*/
+
+
+
