6f4b81fad0bdfc9cb0f064ac6fca08ae5136fa09
SonofaPancak <epaulard21@hotmail.com>
2022-05-29 12:21:07 -0400

    Installation newman et ajout de batchfile et documents relatif aux tests

---
+++ tests/Personnes/testPersonnes.bat
@@ -0,0 +1,11 @@
+@ECHO off
+CALL newman run BDSOL-API.postman_collection.json --folder 1-Create-all-pers --working-dir %cd% -g workspace.postman_globals.json --export-globals ./workspace.postman_globals.json -e ./Personnes/personnes.postman_environment.json --export-environment ./Personnes/personnes.postman_environment.json -d ./Personnes/Create-DataFile.json --timeout-request 5000
+PAUSE
+CALL newman run BDSOL-API.postman_collection.json --folder 2-Update-all-pers --working-dir %cd% -g workspace.postman_globals.json --export-globals ./workspace.postman_globals.json -e ./Personnes/personnes.postman_environment.json --export-environment ./Personnes/personnes.postman_environment.json -d ./Personnes/Update-DataFile.json --timeout-request 5000
+PAUSE
+CALL newman run BDSOL-API.postman_collection.json --folder 3-Search-all-pers --working-dir %cd% -g workspace.postman_globals.json --export-globals ./workspace.postman_globals.json -e ./Personnes/personnes.postman_environment.json --export-environment ./Personnes/personnes.postman_environment.json -d ./Personnes/Search-DataFile.json --timeout-request 5000
+PAUSE
+CALL newman run BDSOL-API.postman_collection.json --folder 4-List-all-pers --working-dir %cd% -g workspace.postman_globals.json --export-globals ./workspace.postman_globals.json -e ./Personnes/personnes.postman_environment.json --export-environment ./Personnes/personnes.postman_environment.json -d ./Personnes/List-DataFile.json --timeout-request 5000
+PAUSE
+CALL newman run BDSOL-API.postman_collection.json --folder 5-Delete-all-pers --working-dir %cd% -g workspace.postman_globals.json --export-globals ./workspace.postman_globals.json -e ./Personnes/personnes.postman_environment.json --export-environment ./Personnes/personnes.postman_environment.json -n 5 --timeout-request 5000
+PAUSE
