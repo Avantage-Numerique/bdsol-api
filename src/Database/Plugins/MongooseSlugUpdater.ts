94d45bfa8f2020d17f269c3eedca3969908487d3
mamarmite <marcandre@mamarmite.com>
2022-08-18 10:22:27 -0400

    Changement du path pour le script serve dans le package.json et changement du chemin de fichier pour le plugin MongooseSlugUpdater.ts

--- src/Database/Plugins/MongooseSlugUpdater.ts
+++ src/Database/Plugins/MongooseSlugUpdater.ts
@@ -6,7 +6,8 @@ export class MongooseSlugUpdater {
     public plugin:any;
 
     public async loadDependancy() {
-        this.plugin = require('../../../node_modules/mongoose-slug-updater');
+        //this.plugin = require('../../../node_modules/mongoose-slug-updater');
+        this.plugin = require('mongoose-slug-updater');
     }
 
     public assign(mongoose:any):any {
