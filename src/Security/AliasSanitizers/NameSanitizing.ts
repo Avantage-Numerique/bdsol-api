88d81f6f3cb543596038b2c01e6732019282276d
mamarmite <marcandre@mamarmite.com>
2022-09-07 15:09:07 -0400

    début du travail pour faire des santiziations regroupé par type de champs.

--- src/Security/AliasSanitizers/NameSanitizing.ts
+++ src/Security/AliasSanitizers/NameSanitizing.ts
@@ -1,12 +1,16 @@
-import {CustomSanitizer} from "express-validator";
-import {UrlSanitizer} from "../Sanitizers/UrlSanitizer";
-import {NoHtmlSanitizer} from "../Sanitizers/NoHtmlSanitizer";
-import {NoSpaceSanitizer} from "../Sanitizers/NoSpaceSanitizer";
-import {NoAccentSanitizer} from "../Sanitizers/NoAccentSanitizer";
-import {LatinSanitizer} from "../Sanitizers/LatinSanitizer";
+//import {CustomSanitizer} from "express-validator";
+//import {UrlSanitizer} from "../Sanitizers/UrlSanitizer";
+//import {NoHtmlSanitizer} from "../Sanitizers/NoHtmlSanitizer";
+//import {NoSpaceSanitizer} from "../Sanitizers/NoSpaceSanitizer";
+//import {NoAccentSanitizer} from "../Sanitizers/NoAccentSanitizer";
+//import {LatinSanitizer} from "../Sanitizers/LatinSanitizer";
 
 
 class NameSanitizing {
+
+    public sanitize(raw:string):string {
+        return raw;
+    }
 
     public getSteps():Array<any> {
         return [
