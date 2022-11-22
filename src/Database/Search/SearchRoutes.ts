87c98d9972720094adb14fd8217d16414c9357ad
SonofaPancak <epaulard21@hotmail.com>
2022-11-22 14:19:11 -0500

    Ajout du domaine search et la route /search avec params

---
+++ src/Database/Search/SearchRoutes.ts
@@ -0,0 +1,39 @@
+import express, {Request, Response} from "express";
+import { StatusCodes } from "http-status-codes";
+
+class SearchRoutes {
+
+    public routerInstance: express.Router;
+    public routerInstanceAuthentification: express.Router;
+
+    constructor(){
+        this.routerInstance = express.Router();
+        this.routerInstanceAuthentification = express.Router();
+    }
+
+    /**
+     * Public routes init
+     * Setup all the endpoint that can be reachable when no token is added to the header (public)
+     * @return {express.Router} router for the public routes
+     * @public @method
+     */
+    public setupPublicRoutes(): express.Router {
+        this.routerInstance.get('/', this.getSearchOnParam);
+        return this.routerInstance;
+    }
+
+    public setupAuthRoutes(): express.Router { return this.routerInstance }
+
+    /**
+     * GetStatusEnumHandler
+     * Handle the return of the Enum of status available
+     * @param req {Request}
+     * @param res {Response}
+     * @param next {NextFunction}
+     * @return {Promise<any>}
+     */
+     public async getSearchOnParam(req: Request, res: Response): Promise<any> {
+        return res.status(StatusCodes.OK).send(req.query);
+    }
+}
+export default SearchRoutes
