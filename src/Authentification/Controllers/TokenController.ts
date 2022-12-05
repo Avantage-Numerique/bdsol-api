6b50ccd0083530541870a87fa1d1b677ca11c134
SonofaPancak <epaulard21@hotmail.com>
2022-12-05 12:54:54 -0500

    Ajustement du findOne au login, car le DTO enlevait le password, et il était impossible de se connecté car l'association username--password ne fonctionnait pas

--- src/Authentification/Controllers/TokenController.ts
+++ src/Authentification/Controllers/TokenController.ts
@@ -84,7 +84,7 @@ export class TokenController {
             // could be : TokenExpiredError
             throw err;
         }
-
+        console.log("##############################", decoded);
         if (TokenController.isValid(decoded) &&
             TokenController.isActive(decoded))
         {
@@ -130,7 +130,7 @@ export class TokenController {
             verifiedToken.iat >= 0 &&
             verifiedToken.exp !== undefined &&
             verifiedToken.exp >= 0 &&
-            verifiedToken.id !== undefined &&
+            (verifiedToken._id !== undefined || verifiedToken.id !== undefined) &&
             verifiedToken.username !== undefined &&
             verifiedToken.role !== undefined;
     }
