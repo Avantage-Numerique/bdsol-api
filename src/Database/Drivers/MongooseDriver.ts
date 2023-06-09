961a4a2fd4f9e61c7058daa27f9e8cc61f7dad26
mamarmite <marcandre@mamarmite.com>
2023-06-09 16:31:11 -0400

    (Fix) : seed db activé en dev et changement d'import pour le fix du build

--- src/Database/Drivers/MongooseDriver.ts
+++ src/Database/Drivers/MongooseDriver.ts
@@ -91,7 +91,7 @@ export class MongooseDBDriver implements DBDriver {
         this.providers.data.assign(OrganisationsService.getInstance(Organisation.getInstance()));
         this.providers.data.assign(ProjectsService.getInstance(Project.getInstance()));
 
-        //await this.seedDB();
+        await this.seedDB();
     }
 
     public async setupIndexes() {
--- src/Database/Migrations/MigrationTemplateSeeder.ts
+++ src/Database/Migrations/MigrationTemplateSeeder.ts
@@ -3,7 +3,7 @@ import {getDbDriver} from "@database/Migrations/MigrationDbConnexion";
 import SeederTaskContract from "@database/Seeders/SeederTaskContract";
 import {TaxonomiesPersistantData} from "@src/Taxonomy/TaxonomiesPersistantData";
 import {taskSeeder} from "@database/Migrations/MigrationTaskSeeder";
-import SeedData from "@database/Seeders/seed-data";
+import SeedData from "@database/Seeders/SeedData";
 import {DBDriver} from "@database/Drivers/DBDriver";
 
 export async function up(): Promise<void> {
