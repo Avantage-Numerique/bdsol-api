512d19968caef37350a1e7efec509a5d7ead9ebf
mamarmite <marcandre@mamarmite.com>
2023-07-28 18:43:29 -0400

    (Ajout) : AJout de node-scheduler avec un manager pour accomplir des tâches au fil du temps.

---
+++ src/Schedule/Jobs/TestJob.ts
@@ -0,0 +1,10 @@
+
+export const TestJob = () => {
+    console.log('/42, The answer to life, the universe, and everything!');
+}
+
+//j.cancel();
+//job.cancelNext(reschedule)
+
+//name
+//description.
