fafb4ba1c1f37ca7864f0d6f7bb0d6b0f9b56745
mamarmite <marcandre@mamarmite.com>
2023-10-13 23:49:24 -0400

    (modification) : changement du scope de responsabilité pour les templates courriels (séparer des notifications). (Ajout) : début de template de base pour les vues public de l'API.

--- src/Notifications/EmailNotification.ts
+++ src/Notifications/EmailNotification.ts
@@ -1,10 +1,11 @@
 import config from "@src/config";
 import {createTransport, Transporter} from 'nodemailer';
 import Notification, {NotificationConfig, NotificationContent} from "@src/Notifications/Notification";
-import * as Nunjucks from "nunjucks";
+import EmailTemplate from "@src/Templates/EmailTemplate";
+
 
 /**
- * Send an email as a notification with the default templates
+ * Send an email as a notification with the default Enailtemplate
  * Some notes in ./notes.md
  * @param config {NotificationConfig} Configuration to send the email. Lost are already defaulted, but need the recipient.
  * @param content {NotificationContent} all the content things
@@ -15,37 +16,26 @@ import * as Nunjucks from "nunjucks";
 class EmailNotification extends Notification {
 
     private _transporter:Transporter;
+    private _emailTemplate:EmailTemplate;
 
     constructor(config:NotificationConfig, content:NotificationContent) {
         super(config, content);
+        this._emailTemplate = new EmailTemplate();
     }
 
     public async send() {
-
-        const templateSystem = Nunjucks.configure("Templates/Emails", {autoescape:true});
-        const renderedTemplate = Nunjucks.render('base.njk', this.content.context);
 
         await this.transporter.sendMail({
             from: config.notifications.email.from,
             to: this.config.recipient,
             subject: this.content.title,
-            html: await this.renderTemplate(),
+            html: await this._emailTemplate.render(this.content),
         });
     }
 
-    public async preview() {
-        return this.renderTemplate();
+    public async preview():Promise<string> {
+        return await this._emailTemplate.preview(this.content);
     }
-
-    public async renderTemplate() {
-        const templateSystem = Nunjucks.configure(`${config.basepath}src/Templates/Emails/`, {autoescape:true, dev:true});
-        console.log("preview", this.content.context, templateSystem);
-
-        return Nunjucks.render(`${config.basepath}src/Templates/Emails/Layouts/default.njk`, {...this.baseTemplateContext, ...this.content.context});
-    }
-
-    //assign variables ?
-    //build template from html.
 
     public get transporter() {
         if (this._transporter === undefined) {
@@ -62,48 +52,6 @@ class EmailNotification extends Notification {
             this._transporter = createTransport(transportOptions);
         }
         return this._transporter;
-    }
-
-    public get baseTemplateContext() {
-        return {
-            app: {
-                name: "AVNU",
-                version: config.version
-            },
-            company: {
-                label: "Est une initiative de",
-                name: "Avantage numérique et le Petit théâtre du vieux Noranda",
-                address: "7e rue Rouyn-Noranda, (QC) J9X 1Z9",
-                phone: "1 (819) 797-6436",
-            },
-            links: {
-                unsuscribe: "https://avnu.ca",
-                cta: "https://avnu.ca",
-                seeAsWeb: "https://avnu.ca"
-            },
-            lang: {
-                ctaLabel: "Confirmer mon compte",
-                copyCta: "Ou vous pouvez copier l'url : ",
-                unsuscribeLabel: "Se désinscrire",
-                seeAsWebLabel: "View as a Web Page"
-            },
-            ...this.theme
-        }
-    }
-
-    public get theme() {
-        return {
-            theme: {
-                bg: "#F7F7F9",
-                color: "#222222",
-                titleColor: "#555555",
-                buttons: {
-                    radius: "20px",
-                    bg: "#222222",
-                    color: "#F7F7F9"
-                }
-            }
-        }
     }
 }
 
