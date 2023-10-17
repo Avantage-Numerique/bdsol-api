6f6e888852d1f29a0e9b06d6abb8fad6833e7a5b
SonofaPancak <epaulard21@hotmail.com>
2023-10-17 12:31:49 -0400

    Ajout du isVerified true sur les fake users

--- src/Data/FakeEntities/fakeUser.ts
+++ src/Data/FakeEntities/fakeUser.ts
@@ -7,7 +7,8 @@ export const fakeUser:Array<UserContract> = [
     email: 'datageek@test.com',
     avatar: 'https://www.pngarea.com/pngm/2/4987230_pepe-png-ez-twitch-emote-png-png-download.png',
     password: '1234',
-    role: 'admin'
+    role: 'admin',
+    verify:{ isVerified: true}
 },
 {
     username: 'SonofaPancak',
@@ -15,7 +16,8 @@ export const fakeUser:Array<UserContract> = [
     email: 'epaulard21@hotmail.com',
     avatar: 'https://res.cloudinary.com/teepublic/image/private/s--W9Vj2N6H--/t_Preview/b_rgb:36538b,c_limit,f_jpg,h_630,q_90,w_630/v1523212912/production/designs/2570408_0.jpg',
     password: '12345',
-    role: 'admin'
+    role: 'admin',
+    verify:{ isVerified: true}
 },
 {
     username: 'Vincenzo',
@@ -23,7 +25,8 @@ export const fakeUser:Array<UserContract> = [
     email: 'vincenzo.ruelier@gmail.fantastique',
     avatar: 'https://www.startupstreamer.com/wp-content/uploads/2020/11/efe1k7bj1mg41-1024x785.png',
     password: '12345',
-    role: 'admin'
+    role: 'admin',
+    verify:{ isVerified: true}
 },
 {
     username: 'mam',
@@ -31,7 +34,8 @@ export const fakeUser:Array<UserContract> = [
     email: 'marcandre@mamarmite.com',
     avatar: 'https://cdn-image.foodandwine.com/sites/default/files/201408-HD-marmite.jpg',
     password: '12345',
-    role: 'admin'
+    role: 'admin',
+    verify:{ isVerified: true}
 },
 {
     username: 'Simon',
@@ -39,7 +43,8 @@ export const fakeUser:Array<UserContract> = [
     email: 'simondescoteaux@hotmail.fun',
     avatar: 'https://i.ytimg.com/vi/cMHcmReOg3c/hqdefault.jpg',
     password: '12345',
-    role: 'admin'
+    role: 'admin',
+    verify:{ isVerified: true}
 },
 
 ]
