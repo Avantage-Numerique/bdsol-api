53ba952c18f72b0e659b3e41d63d45d43f6caf6a
mamarmite <marcandre@mamarmite.com>
2024-01-31 22:26:41 -0500

    (Fix) : DbProvider.ts connexion on prod because it need promise to work.

--- src/Database/Providers/DbProvider.ts
+++ src/Database/Providers/DbProvider.ts
@@ -132,7 +132,10 @@ export abstract class BaseProvider implements DbProvider {
                 options.authSource = this._driver.config.authSource;
             }
 
-            return mongoose.createConnection(url, options);//.asPromise();//ajout du asPromise avec le
+            if (this.connectionAsPromise) {
+                return await mongoose.createConnection(url, options).asPromise();
+            }
+            return mongoose.createConnection(url, options);
 
         } catch (error) {
             LogHelper.error(`[DB][createMongooseConnection] can't create connection to mongo server with mongoose  on ${this._databaseName}`, error);
