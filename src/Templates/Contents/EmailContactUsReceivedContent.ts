616f377ab8eb1ba22ad68e1369a981e087b3860d
SonofaPancak <epaulard21@hotmail.com>
2024-02-10 18:10:12 -0500

    Ajout d'un Ne pas répondre dans la conclusion des courriels envoyé par l'api

--- src/Templates/Contents/EmailContactUsReceivedContent.ts
+++ src/Templates/Contents/EmailContactUsReceivedContent.ts
@@ -7,7 +7,7 @@ const EmailContactUsReceivedContent:any = (welcomeName:string, link:string, addi
             welcome: `Salut, ${welcomeName}`,//to define from the content.//getter to concatenate ?
             title:"Merci de vos commentaires",
             body: "Nous avons bien reçu votre message.<br/><br/>Notre équipe a été notifié et, au besoin, vous réponderons dès que possible.",
-            conclusion: `<strong>Important</strong> <u>Si vous n’êtes pas à l’origine de ce message</u>, veuillez ignorer ce courriel, et merci de <a href="mailto:${baseData.company.email}" title='Contact ${baseData.company.email}'><u>nous contacter via ${baseData.company.email}</u></a> pour nous avertir de la situation.`,
+            conclusion: `<strong>Important</strong> <u>Si vous n’êtes pas à l’origine de ce message</u>, veuillez ignorer ce courriel, et merci de <a href="mailto:${baseData.company.email}" title='Contact ${baseData.company.email}'><u>nous contacter via ${baseData.company.email}</u></a> pour nous avertir de la situation.<br/><u>Veuillez ne pas répondre à courriel.</u>`,
             signature: "L'équipe d'avnu",
             cta: {
                 link: `${link}`,//to define from the content
--- src/Templates/Contents/EmailPasswordChangedContent.ts
+++ src/Templates/Contents/EmailPasswordChangedContent.ts
@@ -5,7 +5,7 @@ const EmailPasswordChangedContent:any = (welcomeName:string, link:string, additi
             welcome: `Salut, ${welcomeName}`,//to define from the content.//getter to concatenate ?
             title:"Votre mot de passe à été modifié",
             body: "Le mot de passe de votre compte avnu.ca associé à ce courriel a été modifié.<br/><br/>",
-            conclusion: "<br/><br/><strong>Important</strong> <u>Si vous n’êtes pas à l’origine de cette modification sur avnu.ca</u>, veuillez nous contacter via notre courriel pour nous informer de la situation.",
+            conclusion: "<br/><br/><strong>Important</strong> <u>Si vous n’êtes pas à l’origine de cette modification sur avnu.ca</u>, veuillez nous contacter via notre courriel pour nous informer de la situation.<br/><u>Veuillez ne pas répondre à courriel.</u>",
             cta: {
                 link: `${link}`,//to define from the content
                 label: "Se connecter à avnu.ca"
