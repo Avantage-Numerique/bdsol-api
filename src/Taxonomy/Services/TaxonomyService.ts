415ba32c3107d1b451dcf516c824e5679f183832
mamarmite <marcandre@mamarmite.com>
2024-05-17 13:47:06 -0400

    (ajout) : Ajout des statistique dans les metas de taxonomies au front et au backend.

--- src/Taxonomy/Services/TaxonomyService.ts
+++ src/Taxonomy/Services/TaxonomyService.ts
@@ -1,6 +1,5 @@
 import {Service} from "@database/DatabaseDomain";
 import Taxonomy from "../Models/Taxonomy";
-import LogHelper from "@src/Monitoring/Helpers/LogHelper";
 
 class TaxonomyService extends Service
 {
@@ -22,12 +21,8 @@ class TaxonomyService extends Service
     public async embedCount(document:any, results:Array<any>) {
         try {
             const currentCount:Number = results.length;
-            document.meta = {
-                count: currentCount
-            }
+            document.meta.statistics.count = currentCount
             await document.save();
-            LogHelper.info(`[Embedding] Taxonomy entities count ${currentCount} assign with ${document.name} taxonomy`);
-
         } catch(e:any) {
             throw new Error(e);
         }
