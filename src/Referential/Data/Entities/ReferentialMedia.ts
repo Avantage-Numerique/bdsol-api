d6b0d1239144be74f0ad8265d59ce137e6730161
lolcabanon <julesses@gmail.com>
2025-12-03 18:01:49 -0500

    typo

--- src/Referential/Data/Entities/ReferentialMedia.ts
+++ src/Referential/Data/Entities/ReferentialMedia.ts
@@ -2,7 +2,7 @@ import { RefItem } from "../types";
 
 export const refMedia: RefItem = {
     label: "Média",
-    description: "Usage interne seulemet",
+    description: "Usage interne seulement. Référence un média dans la plateforme par son identifiant unique.",
     url: "/media",
     ref: [],
 };
