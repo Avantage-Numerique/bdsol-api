c8f604061235a5570ef18fce0df2a3207b3d6908
lolcabanon <julesses@gmail.com>
2025-12-10 10:41:05 -0500

    màj avec le nouveau type RefEntityOrSchema

--- src/Referential/Data/Entities/ReferentialMedia.ts
+++ src/Referential/Data/Entities/ReferentialMedia.ts
@@ -1,6 +1,6 @@
-import { RefItem } from "../types";
+import { RefEntityOrSchema } from "../types";
 
-export const refMedia: RefItem = {
+export const refMedia: RefEntityOrSchema = {
     label: "Média",
     description: "Usage interne seulement. Référence un média dans la plateforme par son identifiant unique.",
     url: "/media",
