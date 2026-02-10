14947d941fc54772ea980a73f3da69feb33ba328
Marc-André Martin <marcandre@mamarmite.com>
2026-02-10 14:27:37 -0500

    Ajout de la compatibilité pour datascene et alternateName.

--- src/Referential/Data/Properties/RefAlternateName.ts
+++ src/Referential/Data/Properties/RefAlternateName.ts
@@ -1,5 +1,6 @@
 import { RefPropertyPrimitive } from "../types";
 import compatibilitySchemaOrg from "@ref/Data/Compatibility/SchemaOrg";
+import compatibilityDataScene from "@ref/Data/Compatibility/DataScene";
 
 export const refAlternateName: RefPropertyPrimitive = {
     field: "alternateName",
@@ -9,6 +10,13 @@ export const refAlternateName: RefPropertyPrimitive = {
     label: "Nom secondaire",
     cardinality: "0..1",
     description: "Autre nom sous lequel l'entité est également connu.",
-    compatibility: [compatibilitySchemaOrg.getOntologyCompatibilityArray("alternateName")],
+    compatibility: [
+        compatibilitySchemaOrg.getOntologyCompatibilityArray("alternateName"),
+        compatibilityDataScene.getOntologyCompatibilityArray(
+            "alternateName",
+            "alternateName",
+            "https://documentation.datascene.ca/references/show/#5-propriete-spectacle-show-alternatename-nom-alternatif"
+        ),
+    ],
     //note: "",
 };
