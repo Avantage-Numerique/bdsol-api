b795026b52fa56a8d4a3a613bfe0138e919a0238
Marc-André Martin <marcandre@mamarmite.com>
2026-02-26 17:11:08 -0500

    Style généraux et ajout du visuel pour les champs requis.

--- src/Referential/Data/SubSchema/RefSkillGroup.ts
+++ src/Referential/Data/SubSchema/RefSkillGroup.ts
@@ -17,7 +17,7 @@ export const refSkillGroup: RefProperty = {
     description:
         "Groupe de compétences, habiletés et/ou de technologies, tirés de notre base de données, accompagné d'un libellé qui décrit le regroupement.",
 
-    compatibility: [compatibilityAvnu.getOntologyCompatibilityArray("Skillgroup")],
+    compatibility: [compatibilityAvnu.getOntologyCompatibilityArray("skillgroup")],
     type: createRefType("object"),
     ref: [
         {
