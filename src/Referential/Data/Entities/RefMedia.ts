4e83ce3b950a300e48bcf76aa475fe4c0c7da4fc
Marc-André Martin <marcandre@mamarmite.com>
2026-04-21 17:33:54 -0400

    Définition du type Media

--- src/Referential/Data/Entities/RefMedia.ts
+++ src/Referential/Data/Entities/RefMedia.ts
@@ -3,6 +3,12 @@ import { createRefType } from "../utils";
 import ArtsdataCompatibility from "@ref/Data/Compatibility/Artsdata";
 import SchemaOrgCompatibility from "@ref/Data/Compatibility/SchemaOrg";
 import DataSceneCompatibility from "@ref/Data/Compatibility/DataScene";
+import { refType } from "@ref/Data/Properties/RefType";
+import { refName } from "@ref/Data/Properties/RefName";
+import { refUrl } from "@ref/Data/Properties/RefUrl";
+import { refAlt } from "@ref/Data/Properties/RefAlt";
+import { refDescription } from "@ref/Data/Properties/RefDescription";
+import { refLicense } from "@ref/Data/Properties/RefLicense";
 
 export const refMedia: RefProperty = {
     label: "Média",
@@ -12,10 +18,29 @@ export const refMedia: RefProperty = {
     type: createRefType("object"),
     compatibility: [
         ArtsdataCompatibility.getOntologyCompatibilityArray("image", "ImageObject", "https://schema.org/ImageObject"),
-        SchemaOrgCompatibility.getOntologyCompatibilityArray("MediaObject"),
+        SchemaOrgCompatibility.getOntologyCompatibilityArray("ImageObject"),
         DataSceneCompatibility.getOntologyCompatibilityArray("media", "Media"),
     ],
     ref: [
-        //enum ImageLicenceEnum
+        { ...refType },
+        {
+            ...refUrl,
+            cardinality: "1..1",
+        }, //contentUrl,
+        { ...refName, cardinality: "0..1" },
+        { ...refDescription }, //alt == description
+        { ...refAlt }, //alt == alt_text
+        { ...refLicense }, //contentUrl,
+        //enum ImageLicenceEnum // https://schema.org/license
+
+        //title
+
+        //description
+        //disambiguatingDescription
+        //usageInfo
+        //caption
+        //licence //?creditText
+        //fileType
+        //sdDatePublished
     ],
 };
---
+++ src/Referential/Data/Properties/RefLicense.ts
@@ -0,0 +1,25 @@
+import { RefProperty } from "../types";
+import { createPrimitiveUrl, createRefType } from "../utils";
+import ArtsdataCompatibility from "@ref/Data/Compatibility/Artsdata";
+import SchemaOrgCompatibility from "@ref/Data/Compatibility/SchemaOrg";
+import DataSceneCompatibility from "@ref/Data/Compatibility/DataScene";
+
+export const refLicense: RefProperty = {
+    field: "license",
+    ontologyProperty: "avnu:license",
+    type: createRefType("string"),
+    url: createPrimitiveUrl("license"),
+    label: "Licence",
+    cardinality: "0..1",
+    description: "Url vers la licence pour l'image",
+    compatibility: [
+        ArtsdataCompatibility.getOntologyCompatibilityArray("license", "license", "https://schema.org/license"),
+        SchemaOrgCompatibility.getOntologyCompatibilityArray("license"),
+        DataSceneCompatibility.getOntologyCompatibilityArray(
+            "license",
+            "license",
+            "https://documentation.datascene.ca/references/media/#6-propriete-media-media-license-licence"
+        ),
+    ],
+    //note: "",
+};
