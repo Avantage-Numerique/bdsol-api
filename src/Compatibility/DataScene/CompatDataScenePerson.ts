0abcc929cbb8b0d194bdcbe4a22a2e972a18bd12
lolcabanon <julesses@gmail.com>
2026-09-16 18:00:16 -0400

    amélioration contenu compat PersonToDatascene

--- src/Compatibility/DataScene/CompatDataScenePerson.ts
+++ src/Compatibility/DataScene/CompatDataScenePerson.ts
@@ -11,15 +11,20 @@ export const compatibilityDBPersonToDataScene: CompatibilityOntology = {
             name: {
                 fields: ["firstName", "lastName"],
                 source: (doc) => doc.firstName + " " + doc.lastName,
-                description: "Desciption de `name` pour DataScene",
+                description: "Nom complet du contributeur, écrit au long.",
             },
             description: {
                 fields: ["description"],
-                source: (doc) => doc.description,
+                source: (doc) => [{ lang: "fr", value: doc.description }],
             },
             shortDescription: {
                 fields: ["shortDescription"],
                 source: (doc) => doc.shortDescription,
+            },
+            alternateName: {
+                fields: ["firstName", "lastName", "nickname"],
+                source: (doc) => [doc.firstName + " " + doc.lastName, doc.nickname].filter((v) => v),
+                description: "Autres appelations parfois utilisées pour le contributeur.",
             },
             /* media: {
             fields: ["mainImage"],
